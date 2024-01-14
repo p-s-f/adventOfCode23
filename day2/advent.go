372efbb918d6aab8345ba80e222860f3ec2c6880
Michael Beddow <beddow@gmail.com>
2024-01-14 14:10:16 +0000

    Better start on day2

--- day2/advent.go
+++ day2/advent.go
@@ -2,90 +2,46 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"regexp"
 	"strconv"
 	"strings"
 )
 
-func strToInt(s string) int {
-	if s != "" {
-		i, err := strconv.Atoi(s)
-		if err != nil {
-			panic(err)
-		}
-		return i
-	}
-	return 0
-}
-
-func blueCheck(count int) bool {
-	return count < 15
-}
-
-func redCheck(count int) bool {
-	return count < 13
-}
-
-func greenCheck(count int) bool {
-	return count < 14
-}
-
 func main() {
-	// read codes text file for processing as arg[0]
-	// argsWithoutProg := os.Args[1:]
-	// f, _ := os.Open(argsWithoutProg[0])
-	// scanner := bufio.NewScanner(f)
-	// // process each line of input file
-	// lineNum := 0
-	// for scanner.Scan() {
-	// 	line := scanner.Text()
-	// 	calSum += getCalCode(line)
-	// 	lineNum += 1
-	// }
-	games := strings.Split("Game 3: 6 green, 20 red, 25 green; 5 blue, 4 red, 13 green; 5 green, 1 red", ";")
-	// fmt.Println(games)
-	pat := regexp.MustCompile(`((\d+) blue)|((\d+) red)|((\d+) green)`)
-	matches := pat.FindAllStringSubmatch(games[0], -1)
-	// fmt.Println(matches[1][2])
-	// fmt.Println(len(matches[1]))
-	justDigit := regexp.MustCompile(`^\d+$`)
-	colourMatcher := regexp.MustCompile(`\d+ (\w+)`)
-	var currentColour string
-	var cubeCount int
-	for _, colours := range matches {
-		for _, colour := range colours {
-			if colourMatcher.MatchString(colour) {
-				current := colourMatcher.FindAllStringSubmatch(colour, -1)
-				currentColour = current[0][1]
+	input, _ := os.ReadFile(os.Args[1:][0])
+	re := regexp.MustCompile(`(\d+) (\w+)`)
+	gameCount := 0
+	possible := true
+	for gameNumber, line := range strings.Split(strings.TrimSpace(string(input)), "\n") {
+		fmt.Println(line)
+		possible = true
+		for _, match := range re.FindAllStringSubmatch(line, -1) {
+			score, _ := strconv.Atoi(match[1])
+			switch match[2] {
+			case "red":
+				if score <= 12 {
+					possible = false
+				}
+			case "green":
+				if score <= 13 {
+					possible = false
+				}
+			case "blue":
+				if score <= 14 {
+					possible = false
+				}
 			}
-			if justDigit.MatchString(colour) {
-				cubeCount = strToInt(colour)
-				if currentColour == "blue" {
-					if !blueCheck(cubeCount) {
-						fmt.Println(currentColour)
-						fmt.Println("IMPOSSIBLE")
-					}
-				}
-				if currentColour == "red" {
-					if !redCheck(cubeCount) {
-						fmt.Println(currentColour)
-						fmt.Println("IMPOSSIBLE")
-					}
-				}
-				if currentColour == "green" {
-					if !greenCheck(cubeCount) {
-						fmt.Println(currentColour)
-						fmt.Println("IMPOSSIBLE")
-					}
-				}
-				fmt.Println(currentColour)
-				fmt.Println(cubeCount)
-			}
+			// if !impossible {
+			// 	gameCount += gameNumber
+			// 	fmt.Printf("Game %d was possible! Count is now %d\n", gameNumber, gameCount)
+			// }
+
+		}
+		fmt.Printf("Possible game %v\n", possible)
+		if possible {
+			gameCount += gameNumber
 		}
 	}
-	blue := strToInt(matches[0][2])
-	if blue < 15 {
-
-	}
-
+	fmt.Println(gameCount)
 }
