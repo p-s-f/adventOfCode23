36750445529f3ebbbae6a6fbbfbe906331d681d9
Michael Beddow <beddow@gmail.com>
2024-01-15 18:52:08 +0000

    crappy answer for part2 of day2. works but could be a lot neater with some thought for sure!

--- day2/advent.go
+++ day2/advent.go
@@ -9,32 +9,49 @@ import (
 )
 
 func main() {
+
 	input, _ := os.ReadFile(os.Args[1:][0])
 	re := regexp.MustCompile(`(\d+) (\w+)`)
 	gameCount := 0
 	possible := true
+	power := 0
+
 	for gameNumber, line := range strings.Split(strings.TrimSpace(string(input)), "\n") {
 		possible = true
+		minRed := 0
+		minBlue := 0
+		minGreen := 0
 		for _, match := range re.FindAllStringSubmatch(line, -1) {
 			score, _ := strconv.Atoi(match[1])
 			switch match[2] {
 			case "red":
+				if score > minRed {
+					minRed = score
+				}
 				if score > 12 {
 					possible = false
 				}
 			case "green":
+				if score > minGreen {
+					minGreen = score
+				}
 				if score > 13 {
 					possible = false
 				}
 			case "blue":
+				if score > minBlue {
+					minBlue = score
+				}
 				if score > 14 {
 					possible = false
 				}
 			}
 		}
+		power += minRed * minBlue * minGreen
 		if possible {
-			gameCount += (gameNumber + 1)
+			gameCount += gameNumber + 1
 		}
 	}
 	fmt.Println(gameCount)
+	fmt.Println(power)
 }
